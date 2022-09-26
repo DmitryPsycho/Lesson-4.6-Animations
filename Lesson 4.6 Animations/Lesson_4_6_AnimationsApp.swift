51eec61b6a096eeb8796cffd0a8d238af0241c29
DmitryPsycho <kovalenko2511@bk.ru>
2022-09-26 14:36:07 +0300

    Initial Commit

---
+++ Lesson 4.6 Animations/Lesson_4_6_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_6_AnimationsApp.swift
+//  Lesson 4.6 Animations
+//
+//  Created by Psycho on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_6_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
